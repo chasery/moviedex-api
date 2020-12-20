e54c04e8add6abffb57a2646d9bf7d579c4ace02
Ryan Chase <ryan@chasery.com>
2020-12-20 15:54:23 -0800

    Add in basic error handling on the api request

--- server.js
+++ server.js
@@ -14,19 +14,35 @@ function handleMovieRequest(req, res) {
   let response = MOVIES;
 
   if (req.query.genre) {
+    if (typeof req.query.genre !== "string") {
+      return res
+        .status(400)
+        .send("The genre parameter requires a string input.");
+    }
     response = response.filter((movie) =>
       movie.genre.toLowerCase().includes(req.query.genre.toLowerCase())
     );
   }
 
   if (req.query.country) {
+    console.log(req.query.country);
+    if (typeof req.query.country !== "string") {
+      return res
+        .status(400)
+        .send("The country parameter requires a string input.");
+    }
     response = response.filter((movie) =>
       movie.country.toLowerCase().includes(req.query.country.toLowerCase())
     );
   }
 
   if (req.query.avg_vote) {
-    // Do something to response
+    const avg_vote = parseFloat(req.query.avg_vote);
+
+    if (avg_vote > 10 || avg_vote < 0 || isNaN(avg_vote)) {
+      return res.status(400).send("Please provide a number between 0-10.");
+    }
+    response = response.filter((movie) => movie.avg_vote >= avg_vote);
   }
 
   res.json(response);
