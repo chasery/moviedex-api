ccb56fa8663392a4edc25cc6e71d609cd8fff9b2
Ryan Chase <ryan@chasery.com>
2020-12-20 15:05:00 -0800

    Basic express app up and running

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
-console.log('hello')
+const express = require("express");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const cors = require("cors");
+
+const app = express();
+
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(cors());
+
+app.get("/movies", (req, res) => {
+  res.send(200, "You hit the nail on the head");
+});
+
+const PORT = 8000;
+
+app.listen(PORT, () => {
+  console.log(`Listening on http://localhost:${PORT}`);
+});
